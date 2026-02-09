29ff99df6b34afc9edf8f9e44c88015007c30f4f
James Brindley <jamesbrindley8@gmail.com>
2026-02-09 01:58:26 +0000

    Remove theme management from data.js
    Removed theme-related functions and constants.

--- js/data.js
+++ js/data.js
@@ -1,5 +1,4 @@
 const STORAGE_KEY = "boardGames";
-const THEME_KEY = "theme";
 
 function getGames() {
   return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
@@ -8,15 +7,3 @@ function getGames() {
 function saveGames(games) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(games));
 }
-
-function setTheme(theme) {
-  document.documentElement.setAttribute("data-theme", theme);
-  localStorage.setItem(THEME_KEY, theme);
-}
-
-function loadTheme() {
-  const theme = localStorage.getItem(THEME_KEY) || "light";
-  setTheme(theme);
-}
-
-loadTheme();
