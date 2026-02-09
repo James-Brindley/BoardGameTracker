b166947103b7ca448874a29adec0629fdcdf04b9
James Brindley <jamesbrindley8@gmail.com>
2026-02-09 19:51:08 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,9 +1,22 @@
 const GAME_KEY = "boardGames";
 
-function getGames() {
+export function getGames() {
   return JSON.parse(localStorage.getItem(GAME_KEY)) || [];
 }
 
-function saveGames(games) {
+export function saveGames(games) {
   localStorage.setItem(GAME_KEY, JSON.stringify(games));
+}
+
+export function todayKey() {
+  return new Date().toISOString().split("T")[0];
+}
+
+export function addPlay(game) {
+  if (!game.playHistory) game.playHistory = {};
+
+  const today = todayKey();
+  game.playHistory[today] = (game.playHistory[today] || 0) + 1;
+
+  game.plays = Object.values(game.playHistory).reduce((a, b) => a + b, 0);
 }
