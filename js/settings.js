f51a4d8d90624f4cc4b1f1ca9efb4bf1e083fef9
James Brindley <jamesbrindley8@gmail.com>
2026-02-09 02:04:58 +0000

    Delete js/settings.js

--- js/settings.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("themeToggle").onclick = () => {
-  const current = document.documentElement.getAttribute("data-theme");
-  const next = current === "dark" ? "light" : "dark";
-
-  document.documentElement.setAttribute("data-theme", next);
-  localStorage.setItem("theme", next);
-};
