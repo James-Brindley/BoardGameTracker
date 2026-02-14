ba516450821d9eaabdf7e828222cf9f2ae7879e1
James Brindley <jamesbrindley8@gmail.com>
2026-02-14 06:13:13 +0000

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'funbvnfgdrcjkykxdyst'
+// Your project URL based on the ID you shared
+const supabaseUrl = 'https://funbvnfgdrcjkykxdyst.supabase.co'
+
+// Copy the long string from the 'anon' 'public' box on that page
 const supabaseKey = 'sb_publishable_9hs47u63lACuH6oviQ4FhA_345LjDYJ'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
