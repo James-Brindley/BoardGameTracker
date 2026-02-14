df453a6115ba3caa610cf7f37e067fc2b9d17f97
James Brindley <jamesbrindley8@gmail.com>
2026-02-14 04:42:24 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,19 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { 
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Your config
+import { 
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDrfFeidoLWHq5sNw1lB59RsAzrc0VG298",
   authDomain: "boardgame-tracker-3f59c.firebaseapp.com",
@@ -13,8 +23,18 @@ const firebaseConfig = {
   appId: "1:490483164045:web:d47116f775572b568513c4"
 };
 
-// Initialize
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export const provider = new GoogleAuthProvider();
+
+export {
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  doc,
+  getDoc,
+  setDoc
+};
