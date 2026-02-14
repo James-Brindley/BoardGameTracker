7e64e5568e383c388b6f08b2d1971a089a968fba
James Brindley <jamesbrindley8@gmail.com>
2026-02-14 04:28:16 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,28 +1,86 @@
-const GAME_KEY = "boardGames";
+import { db, auth } from "./firebase.js";
+import {
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const COLLECTION_NAME = "games";
+
+/* ----------------------------
+   WAIT FOR AUTH
+---------------------------- */
+function waitForUser() {
+  return new Promise(resolve => {
+    const unsub = auth.onAuthStateChanged(user => {
+      if (user) {
+        unsub();
+        resolve(user);
+      }
+    });
+  });
+}
+
+/* ----------------------------
+   MIGRATION SAFETY
+---------------------------- */
 function migrateGame(g) {
-  const migrated = { ...g };
+  return {
+    badges: [],
+    playHistory: {},
+    players: { min: null, max: null },
+    playTime: { min: null, max: null },
+    plays: 0,
+    rating: null,
+    review: "",
+    image: null,
+    ...g
+  };
+}
 
-  migrated.badges ||= [];
-  migrated.playHistory ||= {};
-  migrated.players ||= { min: null, max: null };
-  migrated.playTime ||= { min: null, max: null };
+/* ----------------------------
+   GET GAMES
+---------------------------- */
+export async function getGames() {
+  const user = await waitForUser();
+  const snap = await getDocs(
+    collection(db, "users", user.uid, COLLECTION_NAME)
+  );
 
-  if (typeof migrated.plays !== "number") {
-    migrated.plays = Object.values(migrated.playHistory)
-      .reduce((a,b)=>a+b,0);
+  return snap.docs.map(d =>
+    migrateGame({ id: d.id, ...d.data() })
+  );
+}
+
+/* ----------------------------
+   SAVE ALL GAMES
+---------------------------- */
+export async function saveGames(games) {
+  const user = await waitForUser();
+
+  const existingSnap = await getDocs(
+    collection(db, "users", user.uid, COLLECTION_NAME)
+  );
+
+  const existingIds = existingSnap.docs.map(d => d.id);
+  const newIds = games.map(g => g.id);
+
+  /* Remove deleted */
+  for (const id of existingIds) {
+    if (!newIds.includes(id)) {
+      await deleteDoc(
+        doc(db, "users", user.uid, COLLECTION_NAME, id)
+      );
+    }
   }
 
-  return migrated;
-}
-
-export function getGames() {
-  const raw = JSON.parse(localStorage.getItem(GAME_KEY)) || [];
-  const migrated = raw.map(migrateGame);
-  localStorage.setItem(GAME_KEY, JSON.stringify(migrated));
-  return migrated;
-}
-
-export function saveGames(games) {
-  localStorage.setItem(GAME_KEY, JSON.stringify(games));
+  /* Upsert */
+  for (const g of games) {
+    await setDoc(
+      doc(db, "users", user.uid, COLLECTION_NAME, g.id),
+      { ...g }
+    );
+  }
 }
