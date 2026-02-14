cc85314673217f66ecb4787db8c756aa0cf59c2e
James Brindley <jamesbrindley8@gmail.com>
2026-02-14 05:23:07 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,9 @@
+// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { 
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { 
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+// Your config
 const firebaseConfig = {
   apiKey: "AIzaSyDrfFeidoLWHq5sNw1lB59RsAzrc0VG298",
   authDomain: "boardgame-tracker-3f59c.firebaseapp.com",
@@ -23,18 +13,39 @@ const firebaseConfig = {
   appId: "1:490483164045:web:d47116f775572b568513c4"
 };
 
+// Initialize
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
 export const provider = new GoogleAuthProvider();
 
-export {
-  signInWithPopup,
-  onAuthStateChanged,
-  signOut,
-  doc,
-  getDoc,
-  setDoc
-};
+// UI Helpers
+export function login() {
+  return signInWithPopup(auth, provider);
+}
+
+export function logout() {
+  return signOut(auth);
+}
+
+export function onUserChange(callback) {
+  onAuthStateChanged(auth, callback);
+}
+
+// Firestore helpers
+export async function getUserDoc(uid) {
+  const ref = doc(db, "users", uid);
+  const snap = await getDoc(ref);
+  if (snap.exists()) return snap.data();
+  else return null;
+}
+
+export async function setUserDoc(uid, data) {
+  const ref = doc(db, "users", uid);
+  await setDoc(ref, data, { merge: true });
+}
+
+export async function updateUserDoc(uid, data) {
+  const ref = doc(db, "users", uid);
+  await updateDoc(ref, data);
+}
