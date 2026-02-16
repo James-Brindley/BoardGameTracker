6858d6cfa5b80e0c566ba402c919bce29906b5a0
James Brindley <jamesbrindley8@gmail.com>
2026-02-16 05:46:59 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "gametracker-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/catalogue.html",
+  "/game.html",
+  "/settings.html",
+  "/login.html",
+  "/css/style.css",
+  "/js/theme.js",
+  "/js/data.js",
+  "/js/catalogue.js",
+  "/js/game.js",
+  "/js/stats.js",
+  "/js/supabaseClient.js"
+];
+
+// 1. Install Service Worker
+self.addEventListener("install", (evt) => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Activate & Clean up old caches
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 3. Fetch (Network First, then Cache)
+// This ensures you always see updates immediately when online.
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    fetch(evt.request)
+      .then((res) => {
+        // Clone response to update cache for next time
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(evt.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => {
+        // If offline, try to serve from cache
+        return caches.match(evt.request);
+      })
+  );
+});
